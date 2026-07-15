335ca47c9ef34aae0bbcace2e5f70e11550aa14d
Hashimoto1a <newtondainercia@gmail.com>
2026-07-15 17:24:49 -0300

    Seller TableView

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -115,6 +115,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+			
 		}
 	}
 
